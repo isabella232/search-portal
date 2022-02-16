4adcb270399695ff707acae16fd2acd386508519
Dylan Fong <dylanfong.ut@gmail.com>
2022-02-16 17:54:16 -0500

    fix issue where the loading screen would render infinitely after pressing the back button - this loading spinner logic only checked for results to be null, it did not check the other flag isFetching or check if the searchResults were null

--- src/components/Results/TabContent.jsx
+++ src/components/Results/TabContent.jsx
@@ -4,9 +4,10 @@ import Ndex from './Ndex'
 import LoadingPanel from '../LoadingPanel'
 
 const TabContent = props => {
-  const { results } = props
+  const { results, search } = props;
+  const { isFetching, searchResults } = search;
 
-  if (results === null || results === undefined) {
+  if (results == null && (isFetching || searchResults == null)) {
     return <LoadingPanel title={'Loading results'} message={<div style={{ color: 'black' }}>Please wait...</div>} />
   } else {
     return (
